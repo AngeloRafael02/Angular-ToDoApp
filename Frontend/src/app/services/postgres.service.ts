eff909e5b15c269ac3bcf007743ad078fbe25541
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-03-23 01:06:09 +0800

    Added NestJS Endpoints

--- Frontend/src/app/services/postgres.service.ts
+++ Frontend/src/app/services/postgres.service.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { categoriesInterface, conditionInterface, taskInterface, taskViewInterface } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostgresService {
 
-  constructor() { }
+  private nestJS:string = 'http://localhost:3000';
 
-  public ToDoListSampleData(){
-    let datas= [
-      ["id","Task Title"],
-      [1,"Finish This ToDoList"],
-      [2,"Study C"]
-    ];
-    return datas;
+  constructor(
+    private http:HttpClient
+  ) { }
+
+
+  public getAllCategories():Observable<categoriesInterface[]>{
+    return this.http.get<categoriesInterface[]>(`${this.nestJS}/Misc/allCat`);
+  }
+
+  public getAllConditions():Observable<conditionInterface[]>{
+    return this.http.get<conditionInterface[]>(`${this.nestJS}/Misc/allCond`);
+  }
+
+  public getAllTaskByID(id:number):Observable<taskViewInterface[]>{
+    return this.http.get<taskViewInterface[]>(`${this.nestJS}/task/all/${id}`)
+  }
+
+  public getOneTaskByID(id:number):Observable<taskViewInterface>{
+    return this.http.get<taskViewInterface>(`${this.nestJS}/task/${id}`)
+  }
+
+  public getColumnHeaders(table:string):Observable<string[]>{
+    return this.http.get<string[]>(`${this.nestJS}/misc/col/${table}`);
+  }
+
+  public addTask(taskObj:taskInterface){
+    return this.http.post(`${this.nestJS}/task`,taskObj);
   }
 }
