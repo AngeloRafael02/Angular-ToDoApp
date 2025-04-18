9a840f4ccb3a984290b5444e652ce0f9fdca42eb
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-04-18 15:51:18 +0800

    modified Backup SQLs

--- Docs/SQL/init.sql
+++ Docs/SQL/init.sql
@@ -4,10 +4,24 @@ CREATE TABLE users (
     middleName VARCHAR(20),
     lastName VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    passwd_hash VARCHAR(255) NOT NULL,
+    passwdHash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     last_edited TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+INSERT INTO users(
+    "firstName", 
+    "middleName",
+    "lastName", 
+    "email",
+    "passwdHash"
+) VALUES (
+    'Angelo Rafael',
+    'Fruto',
+    'Recio',
+    'recioangelorafael@gmail.com',
+    '1'
+);
+
 
 CREATE TABLE condition(
     id SERIAL PRIMARY KEY,
@@ -15,31 +29,46 @@ CREATE TABLE condition(
 );
 INSERT INTO conditions(stat)
 VALUES
-('Unfinished'),
-('In Progress'),
-('Finished'),
-('Cancelled'),
-('Delayed'),
-('Continuous'),
-('Speculation');
+    ('Unfinished'),
+    ('In Progress'),
+    ('Finished'),
+    ('Cancelled'),
+    ('Delayed'),
+    ('Continuous'),
+    ('On Hold'),
+    ('Speculation');
+
 
 
 CREATE INTO categories(
     id SERIAL PRIMARY KEY,
     cat VARCHAR(20) NOT NULL
 );
-INSERT TABLE categories(cat)
+INSERT INTO categories(cat)
 VALUES
-('Other'),
-('Personal'),
-('Finance'),
-('Knowledge'),
-('Career'),
-('Automotive'),
-('IT'),
-('Government'),
-('Fitness')
+    ('Personal'),
+    ('Knowledge'),
+    ('Career'),
+    ('Automotive'),
+    ('IT'),
+    ('Government'),
+    ('Finance'),
+    ('Estate'),
+    ('Fitness'),
+    ('Health'),
+    ('Other')
 ;
+
+CREATE TABLE threats(
+    id SERIAL PRIMARY KEY,
+    "level" VARCHAR(10)
+);
+INSERT INTO threats("level") VALUES
+('Low'),
+('Medium'),
+('High'),
+('Alarming'),
+('Inevitable');
 
 CREATE TABLE tasks(
     id SERIAL PRIMARY KEY,
@@ -48,7 +77,7 @@ CREATE TABLE tasks(
     cat_id SMALLINT NOT NULL REFERENCES categories(id),
     prio SMALLINT DEFAULT 0,
     stat_id SMALLINT REFERENCES condition(id) DEFAULT 1,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    last_edited TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_edited TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     owner_id INT NOT NULL REFERENCES users(id)
 );
