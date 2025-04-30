80840cecc76293f94da174a8ef41ca2a63fd20aa
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-04-30 23:55:49 +0800

    Added very basic Styling on ToDoList

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 import { RouterOutlet } from '@angular/router';
-import { ToDoFormComponent } from './components/to-do-form/to-do-form.component';
 import { MatDialog,MatDialogRef } from "@angular/material/dialog";
+import { MatButtonModule } from '@angular/material/button';
+
+import { ClockComponent } from './components/clock/clock.component';
+import { ToDoFormComponent } from './components/to-do-form/to-do-form.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
 @Component({
   selector: 'app-root',
   imports: [
+    MatButtonModule, 
     ToDoListComponent,
+    ClockComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- Frontend/src/app/components/to-do-list/to-do-list.component.ts
+++ Frontend/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {  Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ToDoFormComponent } from '../to-do-form/to-do-form.component';
 import { PostgresService } from '../../services/postgres.service';
@@ -16,124 +16,12 @@ import { taskViewInterface } from '../../interfaces';
     CommonModule,
     MatTableModule,
     MatPaginatorModule,
+    MatButtonModule
   ],
-  template:`  
-    <table mat-table [dataSource]="sampleData" class="mat-elevation-z8" id="tasks">
-      <!--- Note that these columns can be defined in any order.
-            The actual rendered columns are set as a property on the row definition" -->
-
-      <!-- Position Column -->
-      <ng-container matColumnDef="ID">
-        <th mat-header-cell *matHeaderCellDef> ID </th>
-        <td mat-cell *matCellDef="let element"> {{element.ID}} </td>
-      </ng-container>
-
-      <!-- Name Column -->
-      <ng-container matColumnDef="Title">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Title </th>
-        <td mat-cell *matCellDef="let element"> {{element.Title}} </td>
-      </ng-container>
-
-      <!-- Weight Column -->
-      <ng-container matColumnDef="Description">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Description </th>
-        <td mat-cell *matCellDef="let element"> {{element.Description}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="Category">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Category </th>
-        <td mat-cell *matCellDef="let element"> {{element.Category}} </td>
-      </ng-container>
-
-      <!-- Symbol Column -->
-      <ng-container matColumnDef="Priority">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Priority </th>
-        <td mat-cell *matCellDef="let element"> {{element.Priority}} </td>
-      </ng-container>
-
-      <!-- Symbol Column -->
-      <ng-container matColumnDef="Threat Level">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Threat Level </th>
-        <td mat-cell *matCellDef="let element"> {{element['Threat Level']}} </td>
-      </ng-container>
-
-            <!-- Symbol Column -->
-      <ng-container matColumnDef="Status">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Threat Level </th>
-        <td mat-cell *matCellDef="let element"> {{element['Threat Level']}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="Deadline">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Deadline </th>
-        <td mat-cell *matCellDef="let element"> {{element.Deadline.toString().slice(0,10)}} </td>
-      </ng-container>
-      <ng-container matColumnDef="Created At">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header> Created At </th>
-        <td mat-cell *matCellDef="let element"> {{element['Created At']}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="Last Edited">
-        <th mat-header-cell *matHeaderCellDef> Last Edited </th>
-        <td mat-cell *matCellDef="let element"> {{element['Last Edited']}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="Options">
-        <th mat-header-cell *matHeaderCellDef>  Options </th>
-        <td mat-cell *matCellDef="let element"> 
-            <button (click)="finishTask(element.ID)">Finish</button>
-            <button (click)="updateTask(element.ID)">Update</button>
-            <button (click)="deleteTask(element.ID)">Delete</button>
-        </td>
-      </ng-container>
-
-      <ng-container matColumnDef="SID">
-        <th mat-header-cell *matHeaderCellDef> SID </th>
-        <td mat-cell *matCellDef="let element"> {{element.SID}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="TID">
-        <th mat-header-cell *matHeaderCellDef> TID </th>
-        <td mat-cell *matCellDef="let element"> {{element.TID}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="UID">
-        <th mat-header-cell *matHeaderCellDef> UID </th>
-        <td mat-cell *matCellDef="let element"> {{element.UID}} </td>
-      </ng-container>
-
-      <ng-container matColumnDef="CID">
-        <th mat-header-cell *matHeaderCellDef> CID </th>
-        <td mat-cell *matCellDef="let element"> {{element.CID}} </td>
-      </ng-container>
-
-
-      <tr mat-header-row *matHeaderRowDef="taskColumns"></tr>
-      <tr mat-row *matRowDef="let row; columns: taskColumns;" [ngClass]="getRowColorClass(row)"></tr>
-    </table>
-    <mat-paginator [pageSizeOptions]="[5, 10, 20]"
-                 showFirstLastButtons
-                 aria-label="Select page of periodic elements">
-    </mat-paginator>
-  `,
-  styles:`
-    table {width: 100%;}
-    td,th{text-align: center;}
-    table#tasks td:first-child,
-    table#tasks th:first-child,
-    table#tasks th:nth-child(9),
-    table#tasks td:nth-child(9),
-    table#tasks th:nth-child(10),
-    table#tasks td:nth-child(10),
-    table#tasks th:nth-child(11) ~ th,
-    table#tasks td:nth-child(11) ~ td{ display:none }
-    .green-row { background-color: lightgreen; color:black; }
-    .yellow-row { background-color: yellow; color:black;}
-    .orange-row { background-color: orange; color:black; }
-    .red-row { background-color: lightcoral; color:black;  }
-    .black-row { background-color: black; color:white;}
-  `
+  templateUrl:'to-do-list.component.html',
+  styleUrls:['to-do-list.component.scss']
 })
-export class ToDoListComponent implements OnInit, AfterViewInit {
+export class ToDoListComponent implements OnInit{
 
   constructor(
     private matDialog:MatDialog,
@@ -142,11 +30,8 @@ export class ToDoListComponent implements OnInit, AfterViewInit {
   ){}
 
   public taskFormDialogRef:MatDialogRef<ToDoFormComponent>
-  public sampleData:taskViewInterface[] = [];
   public taskColumns:string[] = [];
-  @ViewChild(MatPaginator) 
-  private paginator: MatPaginator;
-  public dataSource = new MatTableDataSource<taskViewInterface>(this.sampleData);
+  public dataSource:MatTableDataSource<taskViewInterface, MatPaginator>;
 
 
   ngOnInit(): void {
@@ -155,12 +40,9 @@ export class ToDoListComponent implements OnInit, AfterViewInit {
       this.taskColumns = this.misc.insertArrayAtIndex(this.taskColumns,["Options"],10)
     });
     this.psql.getAllTaskByID(1).subscribe(data => {
-      this.sampleData = data;
+      this.dataSource = new MatTableDataSource<taskViewInterface>(data);
     });
-  }
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
+    
   }
 
   public updateTask(task:number){
@@ -177,21 +59,70 @@ export class ToDoListComponent implements OnInit, AfterViewInit {
   public finishTask(ID:number){
     this.psql.finishOneTask(ID);
   }
+
+  public deadlineFormatHelper(deadline:string):string{
+    return deadline || deadline.trim() !== '' ? deadline.toString().slice(0,10) : '';
+  }
   
-  public getRowColorClass(row: taskViewInterface): string {
-    switch (row['Threat Level'].toString()){
+  public getThreatLevelCellClass(threat:string): string {
+    switch (threat){
       case 'Low':
-        return 'green-row';
+        return 'greenCell';
       case 'Medium':
-        return 'yellow-row';
+        return 'yellowCell';
       case 'High':
-        return 'orange-row';
+        return 'greenCell';
       case 'Alarming':
-        return 'red-row';
+        return 'redCell';
       case 'Inevitable':
-        return 'black-row';
+        return 'aquaCell';
       default:
-        return 'white-row'
+        return 'whiteCell'
+    }
+  }
+
+  public getStatusColorClass(status:string): string {
+    switch (status){
+      case 'Unfinished':
+        return 'greyCell';
+      case 'In Progress':
+        return 'yellowCell';
+      case 'Finished':
+        return 'greenCell';
+      case 'Cancelled':
+        return 'redCell';
+      case 'Delayed':
+        return 'aquaCell';
+      case 'Continuous':
+        return 'lightblueCell';
+      case 'On Hold':
+        return 'coralCell';
+      case 'Speculation':
+        return 'bluevioletCell';
+      default:
+        return 'whiteCell'
+    }
+  }
+
+  public evaluateDate(row:taskViewInterface):string {
+    const dateString =  row.Deadline
+    const inputDate:Date = new Date(dateString);
+    inputDate.setHours(0, 0, 0, 0);
+    const today:Date = new Date();
+    today.setHours(0, 0, 0, 0);
+    const timeDifference:number = today.getTime() - inputDate.getTime();
+    const daysDifference:number = timeDifference / (1000 * 60 * 60 * 24);
+  
+    if (daysDifference === 0) {
+      return 'redRow';
+    } else if (daysDifference >= -5 && daysDifference < 0) {
+      return 'orangeRow';
+    } else if (daysDifference < -5) {
+      return 'greenRow';
+    } else if (daysDifference >= 1) {
+      return 'blackRow';
+    } else {
+      return 'whiteRow';
     }
   }
 
