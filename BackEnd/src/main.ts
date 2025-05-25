25f9dd551e992ed6c4f9d646f8dd42d65c720ffe
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-05-25 10:55:04 +0800

    removed service

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import 'reflect-metadata'
 import * as compression from 'compression';
 import { ConfigService } from '@nestjs/config';
-import { miscService } from './Providers/misc.provider';
 import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
@@ -14,15 +13,11 @@ async function bootstrap() {
   app.enableCors();
   app.use(compression());
   app.enableVersioning({
-  type: VersioningType.HEADER,
-  header: 'V1.5',
-});
+    type: VersioningType.HEADER,
+    header: 'V1.5',
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('NESTJS_PORT');
-
-  
-
-
   await app.listen(port ?? 3000);
 }
 bootstrap();
