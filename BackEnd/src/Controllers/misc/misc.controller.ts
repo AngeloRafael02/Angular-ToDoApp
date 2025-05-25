627e40a2da59254b82ccbffb4e9bbce47746d683
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-05-25 10:56:08 +0800

    Added custom cache-control on Headers

--- BackEnd/src/Controllers/misc/misc.controller.ts
+++ BackEnd/src/Controllers/misc/misc.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Param, UseGuards } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { miscPrefix } from 'src/app.enums';
 import { Categories } from 'src/Entities/categories';
@@ -15,24 +15,28 @@ export class MiscController {
 
   @Get(miscPrefix.allCat)
   @HttpCode(200)
+  @Header('Cache-Control', 'public, max-age=3600')
   public getCategories(): Promise<Categories[]>{
     return this.miscService.findAllCat();
   }
 
   @Get(miscPrefix.allCond)
   @HttpCode(200)
+  @Header('Cache-Control', 'public, max-age=3600')
   public getConsditions():Promise<Conditions[]>{
     return this.miscService.findAllCond();
   }
 
   @Get(miscPrefix.allThreats)
   @HttpCode(200)
+  @Header('Cache-Control', 'public, max-age=3600')
   public getThreats():Promise<Threats[]>{
     return this.miscService.findAllThreats();
   }
 
-  @Get(`${miscPrefix.col}/:table`)
+  @Get(`col/:table`)
   @HttpCode(200)
+  @Header('Cache-Control', 'public, max-age=300')
   public getColumnHeaders(@Param('table') table:string){
     return this.miscService.getColumnNames(table);
   }
