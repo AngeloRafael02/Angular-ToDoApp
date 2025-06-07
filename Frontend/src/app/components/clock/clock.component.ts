c2745d43599efa43b3c14b28a66cdf826180143d
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-06-07 12:23:12 +0800

    Added Date as output

--- Frontend/src/app/components/clock/clock.component.ts
+++ Frontend/src/app/components/clock/clock.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit  } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
-  imports: [],
+  imports: [DatePipe],
   template: `
   <div id="clockContainer">
-    <h3> Current Time: {{hour}}:{{minute}}:{{second}} {{ampm}}</h3>
+    <h3> Today: {{date | date:'fullDate'}} {{hour}}:{{minute}}:{{second}} {{ampm}}</h3>
   </div>
   `,
   styles: `
     h3{text-align:center; padding:20px 0px 20px 0px; color:white;}
   `
 })
-export class ClockComponent implements OnInit  {
+export class ClockComponent implements OnInit, OnDestroy  {
   private daysArray:string[] = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  private date = new Date();
+  private intervalId: any;
+  public date = new Date();
   public hour:any;
   public minute:string="";
   public second:string="";
@@ -22,13 +24,19 @@ export class ClockComponent implements OnInit  {
   public day:string="";
 
   ngOnInit(): void {
-    setInterval(()=> { 
+    this.intervalId = setInterval(()=> { 
      const date = new Date();
      this.updateDate(date);
     },1000);//this will call the update method in each second
     this.day = this.daysArray[this.date.getDay()];
     //getDay() returns the day in integer format, from 0 to 6 then takes the corresponding date from the daysArray
-   }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 
   private updateDate(date:Date):void{
     const hours:number = date.getHours(); // gets the hours from the date
